360242fe637bc2b7ff7a1de0488a44088a160e27
Ivan Tsimbalist <iv-tsim@yandex.ru>
2020-04-02 16:40:07 +0300

    Complete 6th hard lesson

--- script.js
+++ script.js
@@ -5,24 +5,49 @@ const isNumber = function(n) {
 };
 
 const game = function() {
-    const number = Math.floor(Math.random() * 100) + 1;
+    let number = Math.floor(Math.random() * 100) + 1;
+    alert(number);
+    let attempts = 10;
     const iteration = function() {
         console.log(number);
         let guess = +prompt("Угадай число от 1 до 100");
+        attempts--;
+        if (attempts === 0) {
+            if (confirm("Попытки закончились, хотите сыграть еще?")) {
+                number = Math.floor(Math.random() * 100) + 1;
+                attempts = 10;
+                iteration();
+            } else {
+                alert("Жаль, мы могли провести так много времени...");
+                return;
+            }
+        }
         if (!isNumber(guess)) {
             alert("Введи число!");
             iteration();
         } else if (guess < number) {
-            alert("Загаданное число больше");
+            alert("Загаданное число больше, осталось попыток: " + attempts);
             iteration();
         } else if (guess > number) {
-            alert("Загаданное число меньше");
+            alert("Загаданное число меньше, осталось попыток: " + attempts);
             iteration();
+        } else if (+guess === number) {
+            if (confirm("Поздравляю, Вы угадали!!! Хотели бы сыграть еще?")) {
+                number = Math.floor(Math.random() * 100) + 1;
+                attempts = 10;
+                iteration();
+            } else {
+                alert("Жаль, мы могли провести так много времени...");
+                return;
+            }
         } else if (guess === null) {
+            alert("Жаль, мы могли провести так много времени...");
             return;
         }
     };
     return iteration;
 };
+
 const start = game();
+
 start();
